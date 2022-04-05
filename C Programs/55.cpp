05f92affd6278ca2b26bd455fd8b0314e69a614e
matrixusman <maarifians@gmail.com>
2022-04-05 16:38:44 +0500

    55
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/55.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{
+int n;	                                    
+for(n=5; n>=1; n--)	                 
+printf("%d ", n);  
+getch();
+return 0;
+}
+

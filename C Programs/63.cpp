b3abf317edc4d6ee9a11622998e03e1db7bed364
matrixusman <maarifians@gmail.com>
2022-04-05 16:39:34 +0500

    63
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/63.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{ 
+int x = 0, count;                                          
+for (count = 0; count < 3; count++)	
+x += count;	
+printf(" %d \n", x);
+getch();
+return 0;
+}
+

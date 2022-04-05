7d91f9d8e7e9b2da23885260e7585bd47eba2d1d
matrixusman <maarifians@gmail.com>
2022-04-05 16:23:42 +0500

    CPP programs
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/10.3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int height, width, area;	
+height = 5;	
+width = 4;
+area = height * width;
+printf("Area of Square = %d", area); 
+getch();
+return 0;
+}
+

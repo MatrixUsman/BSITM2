4e9d00d83610925a37d8f2f934cddcffaecbfb3b
matrixusman <maarifians@gmail.com>
2022-04-05 16:24:27 +0500

    10.8
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/10.8.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>	
+int main()	
+{	
+char name[]	= "Usman";
+char address[ ] = "Faisalabad.";
+printf("%25s \n",name); 
+printf("%-25s ",address); 
+getch();
+return 0;
+}
+

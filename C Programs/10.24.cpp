47a62ebaba9b1e313fe4b45f233078cb7e9b239d
matrixusman <maarifians@gmail.com>
2022-04-05 16:24:50 +0500

    10.24
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/10.24.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+char c; 
+printf("Enter character: ");
+c= getche();
+printf("\n The ASCII code of %c is %d ", c, c); 
+getch();
+return 0;
+}
+

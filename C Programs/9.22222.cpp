f5a595d772c3023d9db2778553880fa5ebdc2bf0
matrixusman <maarifians@gmail.com>
2022-04-05 16:23:14 +0500

    CPP programs
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/9.22222.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{	
+int a, b;	
+a = 20;
+b = 5;		
+printf("a + b = %d \n",a + b);
+printf("a - b = %d \n",a = b);
+printf("a * b = %d \n",a * b); 
+printf("a / b = %d \n",a / b);
+printf("a %% b = %d \n",a % b);
+getch();
+return 0;
+}
+

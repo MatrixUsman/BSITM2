f2c0da8be166b5a87819b391de4122771ef99423
matrixusman <maarifians@gmail.com>
2022-04-05 16:25:48 +0500

    11.1
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/11.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{
+int marks; 
+printf("Enter your marks.");
+scanf("%d",&marks);
+if(marks >= 40)
+printf("Congratulations You have passed."); 
+getch();
+return 0;
+}
+
+

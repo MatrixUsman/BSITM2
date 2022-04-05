db06915610caebd2423a84ad3fb7ff556d6ebcb7
matrixusman <maarifians@gmail.com>
2022-04-05 16:40:19 +0500

    234
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/234.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number = 6;
+int x = 0;	                                   
+x = number--;	                                     
+printf(" %d\n",x)			
+getch();
+return 0;
+}
+

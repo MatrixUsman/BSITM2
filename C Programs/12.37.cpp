65a3f5c6e9c4e51a676b34b389a0b48736e16c1a
matrixusman <maarifians@gmail.com>
2022-04-05 16:34:13 +0500

    12.37
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/12.37.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ #include <conio.h>
+int main()
+{
+int i, j;
+for(i=1; i<=7; i++) 
+{
+j= i;
+while( j<=7 ) 
+{
+printf("*"); 
+j++;
+}
+printf("\n"); 
+}
+getch();
+return 0;
+}
+

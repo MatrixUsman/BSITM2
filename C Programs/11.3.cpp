1f49bd38758de70ba04ae38e2e65a67837800d72
matrixusman <maarifians@gmail.com>
2022-04-05 16:26:04 +0500

    11.3
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/11.3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{
+int a, b, c, max; 
+printf("Enter first number: "); 
+scanf("%d",&a);
+printf("Enter second number: ");
+ scanf("%d",&b);
+printf("Enter third number: "); 
+scanf("%d",&c);
+max = a; 
+if(b > max)
+max = b; 
+if(c > max)
+max = c;
+printf("The maximum number is %d",max); 
+getch();
+return 0;
+}
+

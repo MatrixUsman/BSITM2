755637376b161f6a85d20b24ad3113ec3a553b5d
matrixusman <maarifians@gmail.com>
2022-04-05 16:25:08 +0500

    10.26
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/10.26.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>	
+int main()	
+{	
+printf("size of char = %d \n", sizeof(char));	
+printf("size of int = %d \n", sizeof(int));	
+printf("size of float = %d \n", sizeof(float));	
+printf("size of long = %d \n", sizeof(long));
+printf("size of double = %d \n", sizeof(double));
+printf("size of long double = %d \n", sizeof(long double)); 
+getch();
+return 0;
+}
+

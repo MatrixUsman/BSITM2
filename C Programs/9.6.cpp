0387e0e124825d17109a9ff65b33038eb00d8644
matrixusman <maarifians@gmail.com>
2022-04-05 16:22:51 +0500

    CPP programs
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/9.6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{
+int a, b, x, y;
+a=b=x=y=0; 
+a--;
+b = a;
+--x;
+y = x; 
+printf("a = %d \n b = %d \n", a, b);
+printf("x = %d \n y = %d \n", x, y);
+getch();
+return 0;
+}
+

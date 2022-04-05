2a3dc7b94a4956c6a3deb56adb1636ae79baf6c4
matrixusman <maarifians@gmail.com>
2022-04-05 16:28:10 +0500

    11.22
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/11.22.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+int main()	
+{	
+int a, b;	
+char op; 
+printf("Enter 1st number, operator and 2nd number:"); 
+scanf("%d %c %d", &a, &op, &b);
+switch(op)
+{
+case '+':
+printf("%d + %d = %d",a,b,a+b);
+ break;
+case '-':
+printf("%d - %d = %d",a,b,a-b);
+ break;
+case '*':
+printf("%d * %d = %d",a,b,a*b);
+ break;
+case '/':
+printf("%d / %d = %d",a,b,a/b); 
+break;
+case '%':
+printf("%d %% %d = %d",a,b,a%b);
+ break;
+default:
+printf("Invalid operator!"); 
+}
+getch(); 
+return 0;
+}
+

f2766c8187d56eff53674e3d7dd1c40ba60695d3
matrixusman <maarifians@gmail.com>
2022-04-05 16:25:55 +0500

    11.2
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/11.2.cpp
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+ #include <conio.h> 
+int main()
+{
+int a, b;
+printf("Enter a number");
+scanf("%d",&a);
+printf("Enter a number ");
+scanf("%d",&b);
+if(a *a == b)
+printf("2nd number is square of 1st number.");
+getch();
+return 0;
+}
+

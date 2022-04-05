ec27b364cce0ebea00b03db7a62c747c0dd034d3
matrixusman <maarifians@gmail.com>
2022-04-05 16:39:00 +0500

    58
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ C Programs/58.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{ 
+int x = 5, y = 3;	                           
+do	                                      
+{	
+x=x*2;	
+y = y + 2;	                           
+}
+ while (y < 7);	                     
+printf(" %d ", x);	
+getch();
+return 0;
+}
+
+
+

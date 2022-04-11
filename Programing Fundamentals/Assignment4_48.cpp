57115e0598f7a622e38586c182f34d300776dfcb
matrixusman <maarifians@gmail.com>
2022-04-12 00:01:31 +0500

    more
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ Programing Fundamentals/Assignment4_48.cpp
@@ -0,0 +1,24 @@
+//  Assignment 4
+//  Created by Usman Rizwan on 07/12/2020.
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age, marks;
+    float percent;
+
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    cout<<"Enter your percentage: ";
+    cin>>percent;
+    
+    cout<<"Your name is: "<<name<<" "<<endl;
+    cout<<"Your age is: "<<age<<endl;
+    cout<<"Your marks are: "<<marks<<endl;
+    cout<<"Your percentage is: "<<percent;
+}
---
+++ Programing Fundamentals/Assignment6_13.cpp
@@ -0,0 +1,27 @@
+//  Assignment 6
+//  Created by Usman Rizwan on 15/12/2020.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable Declaration
+    int a;
+    int b;
+    
+    // Assigning Values
+    
+    cout<<"Enter Your first number: ";
+    cin>>a;
+    cout<<"Enter Your Second number: ";
+    cin>>b;
+    
+    
+    if (a == b)
+    {
+        cout<<"Both numbers are equal"<<endl;
+    }
+    else{
+        cout<<"Both numbers are not equal"<<endl;
+    }
+}

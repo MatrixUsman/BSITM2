19d051bb5d842ccb244f0b4bbe543b014892c193
matrixusman <maarifians@gmail.com>
2022-12-13 13:36:39 +0500

    DBMS
    Signed-off-by: matrixusman <maarifians@gmail.com>

---
+++ DBMS/Presentation and assignments/dbms.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12.6 (x86_64)
+--
+-- Host: localhost    Database: dbmss
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Empl`
+--
+
+DROP TABLE IF EXISTS `Empl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Empl` (
+  `Empno` int NOT NULL,
+  `Ename` varchar(30) NOT NULL,
+  `Job` varchar(10) DEFAULT NULL,
+  `Mgr` int DEFAULT NULL,
+  `Hiredate` date DEFAULT NULL,
+  `Sal` decimal(10,0) DEFAULT NULL,
+  `Comm` decimal(10,0) DEFAULT NULL,
+  `Deptno` int DEFAULT NULL,
+  PRIMARY KEY (`Empno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Empl`
+--
+
+LOCK TABLES `Empl` WRITE;
+/*!40000 ALTER TABLE `Empl` DISABLE KEYS */;
+INSERT INTO `Empl` VALUES (8369,'SMITH','CLERK',8902,'1990-12-18',800,NULL,20),(8499,'ANATA','SALESMAN',8698,'1991-02-20',1600,300,30),(8521,'SETH','SALESMAN',8698,'1991-02-22',1250,500,30),(8566,'MAHADEVAN','MANAGER',8839,'1991-04-02',2985,NULL,20),(8654,'MOMIN','SALESMAN',8698,'1991-09-28',1250,1400,30),(8698,'BINA','MANAGER',8839,'1991-05-01',2850,NULL,30),(8839,'AMIR','PRESIDENT',NULL,'1991-11-18',5000,NULL,10),(8844,'KULDEEP','SALESMAN',8698,'1991-09-08',1500,0,30),(8882,'SHIVANSH','MANAGER',8839,'1991-06-09',2450,NULL,10),(8888,'SCOTT','ANALYST',8566,'1992-12-09',3000,NULL,20);
+/*!40000 ALTER TABLE `Empl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Student`
+--
+
+DROP TABLE IF EXISTS `Student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Student` (
+  `StdID` int NOT NULL,
+  `StdName` varchar(30) NOT NULL,
+  `Sex` varchar(1) DEFAULT NULL,
+  `Percentage` decimal(5,2) DEFAULT NULL,
+  `SClass` int DEFAULT NULL,
+  `Sec` varchar(1) DEFAULT NULL,
+  `Stream` varchar(10) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  PRIMARY KEY (`StdID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Student`
+--
+
+LOCK TABLES `Student` WRITE;
+/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
+INSERT INTO `Student` VALUES (1001,'AKSHRA AGARWAL','F',70.00,11,'A','Science','1196-11-10'),(1002,'ANJANI SHARMA','F',75.00,11,'A','Commerce','1196-09-18'),(1003,'ANSHUL SAXENA','M',78.00,11,'A','Commerce','1196-11-01'),(1004,'AKRITI SAXENA','F',76.00,11,'A','Commerce','1196-09-20'),(1005,'AISHWARYA SINGH','F',76.00,11,'A','Commerce','1196-09-20'),(1006,'KHUSHI AGARWAL','F',77.00,11,'A','Commerce','2003-09-14'),(1007,'MAAHI AGARWAL','F',74.00,11,'A','Science','1997-04-21'),(1008,'MITALI GUPTA','F',78.00,11,'A','Science','1997-11-26'),(1009,'NIKUJ AGARWAL','M',58.00,11,'A','Science','1997-07-12'),(1010,'PARKHI','F',59.00,12,'A','COMMERCE','1997-12-20');
+/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-03 19:46:25

194729092f768eff7d38f117c48987bdc1ebda5f
MatrixUsman <maarifians@gmail.com>
2023-06-19 13:57:53 +0500

    fixing assignment 2
    Signed-off-by: MatrixUsman <maarifians@gmail.com>

--- Semester 6/Android Devlopment/Assignment_2/BSITM22048/app/src/main/java/com/usman/bsit_m2_20_48/MainActivity.java
+++ Semester 6/Android Devlopment/Assignment_2/BSITM22048/app/src/main/java/com/usman/bsit_m2_20_48/MainActivity.java
@@ -8,41 +8,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Button buttonLogin;
-    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-
-                signIn(email, password);
-            }
-        });
-
-
         EditText editTextEmail = findViewById(R.id.editTextEmail);
         editTextEmail.addTextChangedListener(new TextWatcher() {
             @Override
